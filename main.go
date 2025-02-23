bad842a0b617afb285c5033f800dc8582276c446
Bethany Hicks <t3cdr@MacBook-Pro.local>
2025-02-22 20:13:06 -0800

    endpoint added

--- main.go
+++ main.go
@@ -11,7 +11,8 @@ func main() {
 
 	mux := http.NewServeMux()
 
-	mux.Handle("/", http.FileServer(http.Dir(filtpathRoot)))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filtpathRoot))))
+	mux.HandleFunc("/healthz", handlerReadiness)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
@@ -20,4 +21,10 @@ func main() {
 
 	log.Printf("Serving files from %s on port: %s\n", filtpathRoot, port)
 	log.Fatal(srv.ListenAndServe())
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
